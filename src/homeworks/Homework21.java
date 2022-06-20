b230ce5ddc30859c352575e789672ec9a11202b8
kalyno123 <kalyno706@gmail.com>
2022-06-20 13:11:45 -0500

    hw22

--- src/homeworks/Homework21.java
+++ src/homeworks/Homework21.java
@@ -1,5 +1,6 @@
 package homeworks;
 
+import java.util.Arrays;
 
 public class Homework21 {
 
@@ -7,8 +8,8 @@ public class Homework21 {
     This method will take an int argument, and it will print numbers starting from 1 to given argument.
     BUT, it will print “Fizz” for the numbers divided by 3, “Buzz” for the numbers divided by 5, and “FizzBuzz” for the numbers divided both by 3 and 5.
     */
-    public static void fizzBuzz1(int a){
-        for (int i = 1; i <= a; i++){
+    public static void fizzBuzz1(int a) {
+        for (int i = 1; i <= a; i++) {
             if (i % 15 == 0) System.out.println("FizzBuzz");
             else if (i % 3 == 0) System.out.println("Fizz");
             else if (i % 5 == 0) System.out.println("Buzz");
@@ -21,11 +22,11 @@ public class Homework21 {
     This method will take an int argument, and it will return a String. BUT it will return “Fizz” if the given number is divided by 3,
     “Buzz” if the number is divided by 5, and “FizzBuzz” if the number is divided both by 3 and 5. Otherwise, it will return number itself.
     */
-    public static String fizzBuzz2(int a){
-        for (int i = 0; i <= a; i++){
+    public static String fizzBuzz2(int a) {
+        for (int i = 0; i <= a; i++) {
             if (a % 15 == 0) return ("FizzBuzz");
             else if (a % 3 == 0) return ("Fizz");
-            else if (a % 5 == 0) return("Buzz");
+            else if (a % 5 == 0) return ("Buzz");
         }
         return String.valueOf(a);
     }
@@ -35,16 +36,17 @@ public class Homework21 {
     This method will take a String argument, and it will return an int which is the sum of all numbers presented in the String.
     NOTE: If there are no numbers represented in the String, return 0
     */
-    public static int findSumNumbers(String s){
+    public static int findSumNumbers(String s) {
         int sum = 0; // container to hold sum of numbers
         String temp = "0"; // container to hold String format of the numbers
-        for (int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++) {
             if (Character.isDigit(s.charAt(i))) temp += s.charAt(i); // checking if the current char is digit
             else { // else if current char is letter ...
                 sum += Integer.parseInt(temp); // ... then add to the sum of the digits found prior to letter
                 temp = "0"; // reset the String container back to empty
             }
-        }return sum + Integer.parseInt(temp);
+        }
+        return sum + Integer.parseInt(temp);
     }
 
 
@@ -52,12 +54,12 @@ public class Homework21 {
     This method will take a String argument, and it will return an int which is the number presented in the String.
     NOTE: If there are no numbers represented in the String, return 0.
     */
-    public static int findBiggestNumber(String s){
+    public static int findBiggestNumber(String s) {
         int max = 0;
         String temp = s.replaceAll("[^\\d]", " ");
-        String[] temp2 = temp.replaceAll("[\\s]", " ").split(" ");
-        for (String element: temp2){
-            if (!element.isEmpty()){
+        String[] temp2 = temp.replaceAll("[\\s]+", " ").trim().split(" ");
+        for (String element : temp2) {
+            if (!element.isEmpty()) {
                 if (Integer.parseInt(element) > max) max = Integer.parseInt(element);
             }
         }
@@ -69,26 +71,37 @@ public class Homework21 {
     NOTE: If given String is empty, then return empty String.
     NOTE: It is case sensitive!!!
     */
-    public static String countSequenceOfCharacters(String s){
-        String result = "";
-        int count = 0;
-        String str = "";
-        for (int i = 0; i < s.length()-1; i++){
-            if (s.charAt(i) == s.charAt(i+1)){
-                count++;
-                str += String.valueOf(s.charAt(i));
-            }else{
-                result += count;
-                result += str;
-                count = 0;
-                str = "";
+    public static String countSequenceOfCharacters(String s) {
+        /* String sequence = "";
+        int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j))
+                    count++;
+            }
+            if (!sequence.contains(str.charAt(i) + "")) {
+                sequence += str.charAt(i);
+                sequence += count;
+                count = 1;
             }
         }
-        return result;
+        return sequence; */
+
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length()-1 && s.charAt(i) == s.charAt(i+1)){
+                count ++;
+            }
+            else {
+                count ++;
+                result.append(count).append(s.charAt(i));
+                count = 0;
+            }
+        }
+        return result.toString();
+
     }
-
-
-
 
 
     public static void main(String[] args) {
@@ -124,11 +137,11 @@ public class Homework21 {
 
 
 
-        /*String s = "ab110c045d";
+        /* String s = "ab110c045d";
         int sum = 0;
         int max = Integer.MIN_VALUE;
         String temp = s.replaceAll("[^\\d]", " ");
-        String[] temp2 = temp.replaceAll("[\\s]+", " ").split(" ");
+        String[] temp2 = temp.replaceAll("[\\s]+", " ").trim().split(" ");
         for (String element: temp2){
             if (!element.isEmpty()){
                 sum += Integer.parseInt(element);
@@ -137,7 +150,7 @@ public class Homework21 {
         }
         System.out.println(Arrays.toString(temp2));
         System.out.println(sum);
-        System.out.println(max);*/
+        System.out.println(max); */
 
     }
 }
